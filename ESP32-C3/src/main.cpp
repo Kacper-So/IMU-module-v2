5f79fe207063c3800bbb3e517cd6edc2b8fb6bf9
Kacper-So <kacpersobczak152@gmail.com>
2023-09-10 17:52:00 +0200

    env setup and first firmware, read sensors

--- ESP32-C3/src/main.cpp
+++ ESP32-C3/src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <ICM40627.h>
+#include <SparkFun_MMC5983MA_Arduino_Library.h>
+ 
+ICM40627 IMU;
+SFE_MMC5983MA MAG;
 
-// put function declarations here:
-int myFunction(int, int);
+int16_t IMU_data;
+uint32_t currentX = 0;
+uint32_t currentY = 0;
+uint32_t currentZ = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Wire.begin(4,5);
+  IMU.init(AODR_200Hz, GODR_200Hz, AFS_8G, GFS_2000DPS);
+  MAG.begin();
+  MAG.softReset();
+  Serial.begin(115200);
+  Serial.println("\nI2C Scanner");
 }
-
+ 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  IMU.readData(&IMU_data);
+  Serial.println("IMU data:");
+  Serial.println(IMU_data);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  currentX = MAG.getMeasurementX();
+  currentY = MAG.getMeasurementY();
+  currentZ = MAG.getMeasurementZ();
+  Serial.println("MAG data:");
+  Serial.println(currentX);
+  Serial.println(currentY);
+  Serial.println(currentZ);
+  Serial.println();
+
+  delay(1000);          
 }
