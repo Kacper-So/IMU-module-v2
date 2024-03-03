5caee44a369e234f501aa0776daef5d9ff6e5968
Kacper-So <kacpersobczak152@gmail.com>
2024-03-03 18:35:17 +0100

    firmware AHRS

--- ESP32-C3/src/main.cpp
+++ ESP32-C3/src/main.cpp
@@ -1,38 +1,82 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <ICM40627.h>
+#include <MadgwickAHRS.h>
+#include <MahonyAHRS.h>
 #include <SparkFun_MMC5983MA_Arduino_Library.h>
  
 ICM40627 IMU;
 SFE_MMC5983MA MAG;
+Madgwick MadgwickFilter;
+Mahony MahonyFilter;
 
-int16_t IMU_data;
+struct MeasurementData {
+    float accelerometer_x;
+    float accelerometer_y;
+    float accelerometer_z;
+    float gyroscope_x;
+    float gyroscope_y;
+    float gyroscope_z;
+    float mag_x;
+    float mag_y;
+    float mag_z;
+};
+
+struct AHRSData {
+    float roll;
+    float pitch;
+    float yaw;
+};
+
+MeasurementData data;
+AHRSData MadgwickResult;
+AHRSData MahonyResult;
+AHRSData KalmanResult;
+AHRSData ComplementarFilterResult;
+
+void update_measurements(MeasurementData &data){
+  int16_t IMU_data[7];
+  IMU.readData(IMU_data);
+  data.accelerometer_x = IMU_data[1] * IMU.getAres(AFS_8G);
+  data.accelerometer_y = IMU_data[2] * IMU.getAres(AFS_8G);
+  data.accelerometer_z = IMU_data[3] * IMU.getAres(AFS_8G);
+
+  data.gyroscope_x = IMU_data[4] * IMU.getAres(AFS_8G);
+  data.gyroscope_y = IMU_data[5] * IMU.getAres(AFS_8G);
+  data.gyroscope_z = IMU_data[6] * IMU.getAres(AFS_8G);
+
+  data.mag_x = MAG.getMeasurementX();
+  data.mag_y = MAG.getMeasurementY();
+  data.mag_z = MAG.getMeasurementZ();
+}
+
+int16_t IMU_data[7];
 uint32_t currentX = 0;
 uint32_t currentY = 0;
 uint32_t currentZ = 0;
 
 void setup() {
   Wire.begin(4,5);
-  IMU.init(AODR_200Hz, GODR_200Hz, AFS_8G, GFS_2000DPS);
+  IMU.init(AFS_8G, GFS_2000DPS, AODR_200Hz, GODR_200Hz);
   MAG.begin();
   MAG.softReset();
   Serial.begin(115200);
-  Serial.println("\nI2C Scanner");
+
+  MadgwickFilter.begin(200);
+  MahonyFilter.begin(200);
 }
  
 void loop() {
-  IMU.readData(&IMU_data);
-  Serial.println("IMU data:");
-  Serial.println(IMU_data);
 
-  currentX = MAG.getMeasurementX();
-  currentY = MAG.getMeasurementY();
-  currentZ = MAG.getMeasurementZ();
-  Serial.println("MAG data:");
-  Serial.println(currentX);
-  Serial.println(currentY);
-  Serial.println(currentZ);
-  Serial.println();
+  MadgwickFilter.update(data.gyroscope_x, data.gyroscope_y, data.gyroscope_z, data.accelerometer_x, data.accelerometer_y, data.accelerometer_z, data.mag_x, data.mag_y, data.mag_z);
+  MadgwickResult.roll = MadgwickFilter.getRoll() * RAD_TO_DEG;
+  MadgwickResult.pitch = MadgwickFilter.getPitch() * RAD_TO_DEG;
+  MadgwickResult.yaw = MadgwickFilter.getYaw() * RAD_TO_DEG;
 
-  delay(1000);          
+  MahonyFilter.update(data.gyroscope_x, data.gyroscope_y, data.gyroscope_z, data.accelerometer_x, data.accelerometer_y, data.accelerometer_z, data.mag_x, data.mag_y, data.mag_z);
+  MahonyResult.roll = MahonyFilter.getRoll() * RAD_TO_DEG;
+  MahonyResult.pitch = MahonyFilter.getPitch() * RAD_TO_DEG;
+  MahonyResult.yaw = MahonyFilter.getYaw() * RAD_TO_DEG;
+
+  delay(100);
 }
